2ab4395b131901930f63bd2628d545e6a1136e6d
Rajnish Adhikari <rajnishadhikari01@gmail.com>
2022-06-21 20:40:43 +0545

    Add project files.

---
+++ classwork/Controllers/About_usController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace classwork.Controllers
+{
+    public class About_usController : Controller
+    {
+        // GET: About_us
+        
+        public ActionResult DisplayAbout()
+        {
+            return View();
+        }
+
+    }
+}
